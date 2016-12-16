0ffb724dd9a62f6523ff4a07b8a2702661f3aa0c
Zachary Sedefian <zbsedefian@gmail.com>
2016-12-16 02:30:31 -0500

    Customer interval.

--- jscripts/app.js
+++ jscripts/app.js
@@ -1,15 +1,30 @@
-function go(){
+var time;
+
 var currentIndex = 0;
 $(".box:not(:eq("+ currentIndex +"))").hide();
 
 var totalDiv = $(".box").length;
 
-setInterval(function(){
-     currentIndex = (currentIndex + Math.floor(Math.random() * 6)) % totalDiv;
+var timer;
 
-     $(".box").hide();
-     $(".box").eq(currentIndex).show();
-    
-},3000);
+$('#setTime').click(function(){
+    clearInterval(timer);
+    time = $('#time').val();
+    console.log(time);
+    timer = setInterval(function(){
+        currentIndex = (currentIndex + Math.floor(Math.random() * 12)) % totalDiv;
 
-}
+        $(".box").hide();
+        $(".box").eq(currentIndex).show();
+    },time);
+});
+
+
+//setInterval(function(){
+//     currentIndex = (currentIndex + Math.floor(Math.random() * 6)) % totalDiv;
+//
+//     $(".box").hide();
+//     $(".box").eq(currentIndex).show();
+//},time);
+
+
