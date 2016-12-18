7c0693e33953eadb36f1e74ab19deb9fb11b388c
Zachary Sedefian <zbsedefian@gmail.com>
2016-12-18 17:51:54 -0500

    Added string and chord as options

--- jscripts/app.js
+++ jscripts/app.js
@@ -1,14 +1,88 @@
+//use for key circle
 var currentIndex = 0;
-$(".box:not(:eq("+ currentIndex +"))").hide();
-var totalDiv = $(".box").length;
+var totalDiv = $(".key").length;
+
+//use for string circle
+var currentIndexString = 0;
+var totalDivString = $(".string").length;
+
+//use for chord circle
+var currentIndexChord = 0;
+var totalDivChord = $(".chord").length;
+
+//use to convert ms to s
 var timer;
 var time;
+
+//key toggle
+$(".key:not(:eq("+ currentIndex +"))").hide();
+
+//string toggle
+$(".string:not(:eq("+ currentIndexString +"))").hide();
+$('#first').hide();
+
+//chord toggle
+$(".chord:not(:eq("+ currentIndexChord +"))").hide();
+$('#maj7').hide();
+
+//when set time is clicked, starts and restarts interval
 $('#setTime').click(function(){
+//    $(".string").hide();
+//    $(".chord").hide();
     clearInterval(timer);
-    time = $('#time').val();
+    time = $('#time').val() * 1000;
+    $('.buttons').append("<p class='message'>Interval has been set to " + time / 1000 + " seconds.</p>");
+    $('.message').fadeOut(2000);
     timer = setInterval(function(){
+        
         currentIndex = (currentIndex + Math.floor(Math.random() * 12)) % totalDiv;
-        $(".box").hide();
-        $(".box").eq(currentIndex).show();
+        $(".key").hide();
+        $(".key").eq(currentIndex).show();
+        
+        if($('#beep').is(":checked")) {
+            $(".beep-sound")[0].play();
+        }
+        
+        if($('#show-string').is(":checked")){
+            $('.string-label').show();
+            currentIndexString = (currentIndexString + Math.floor(Math.random() * 12)) % totalDivString;
+            $(".string").hide();
+            $(".string").eq(currentIndexString).show();
+        }
+        
+        if($('#show-chord').is(":checked")){
+            $('.chord-label').show();
+            currentIndexChord = (currentIndexChord + Math.floor(Math.random() * 12)) % totalDivChord;
+            $(".chord").hide();
+            $(".chord").eq(currentIndexChord).show();
+        }
+        
     },time);
 });
+
+//listen for checkbox clicking
+$('input:checkbox').change(
+            function(){
+                if ($('#show-string').is(':checked')) {
+                    $(".string-main").show();
+                    $('.string-label').show();
+                    currentIndexString = (currentIndexString + Math.floor(Math.random() * 12)) % totalDivString;
+                    $(".string").hide();
+                    $(".string").eq(currentIndexString).show();
+                } else {
+                    $(".string-main").hide();
+                }
+                
+                if($('#show-chord').is(":checked")){
+                    $(".chord-main").show();
+                    $('.chord-label').show();
+                    currentIndexChord = (currentIndexChord + Math.floor(Math.random() * 12)) % totalDivChord;
+                    $(".chord").hide();
+                    $(".chord").eq(currentIndexChord).show();
+                } else {
+                    $(".chord-main").hide();
+                }
+            });
+
+//if stop is clicked
+$('#stopTime').click(function(){clearInterval(timer)});
